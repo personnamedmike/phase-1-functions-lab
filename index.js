3f1d4a56bf3c66afd4f31f4372fefa3c5e951f8a
David <Davidmaxsands@gmail.com>
2022-01-11 15:53:04 -0700

    commit tree 34f7e8312e3754c5ba2ed57bcaccafba01ed56d6

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const distanceFromHqInBlocks = function (street) {
 function distanceFromHqInFeet(street) {
   return distanceFromHqInBlocks(street) * 264;
 }
+
+function distanceTravelledInFeet(start, destination){
+  return Math.abs(start-destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet(start, destination) <= 400){
+    return 0
+  }
+  else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+    return 2.56
+  } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+    return 25 
+  } else if (distanceTravelledInFeet(start, destination) > 2500) {
+    return 'cannot travel that far'
+  }
+  
+}
